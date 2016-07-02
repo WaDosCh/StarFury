6cc97fc3ccb184955af568e1d8e29a5623acf930
judos <judos@gmx.ch>
2016-07-02 16:07:57 +0200

    working example with opengl sprites

--- src/ch/wados/starfury/controller/GameRenderingManager.java
+++ src/ch/wados/starfury/controller/GameRenderingManager.java
@@ -1,15 +1,86 @@
 package ch.wados.starfury.controller;
 
+import static org.lwjgl.demo.opengl.util.DemoUtils.ioResourceToByteBuffer;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL14.*;
+import static org.lwjgl.opengl.GL30.*;
+import static org.lwjgl.opengl.GL31.*;
+import static org.lwjgl.opengl.GL32.*;
+import static org.lwjgl.opengl.GL33.*;
+import static org.lwjgl.opengl.GL40.*;
+import static org.lwjgl.opengl.GL41.*;
+import static org.lwjgl.stb.STBImage.stbi_failure_reason;
+import static org.lwjgl.stb.STBImage.stbi_image_free;
+import static org.lwjgl.stb.STBImage.stbi_info_from_memory;
+import static org.lwjgl.stb.STBImage.stbi_load_from_memory;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL15;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL30;
+
 public class GameRenderingManager implements RenderingManager {
 
+	private int tex;
+
 	public GameRenderingManager() {
+
 	}
 
 	@Override
 	public void draw() {
+		//
 	}
 
 	@Override
-	public void load() {
+	public void load() throws IOException {
+		loadTexture();
+		loadRenderData();
+	}
+
+	private void loadRenderData() {
+		int vao = glGenVertexArrays();
+		glBindVertexArray(vao);
+
+		FloatBuffer vertices = BufferUtils.createFloatBuffer(3 * 6);
+		vertices.put(-0.6f).put(-0.4f).put(0f).put(1f).put(0f).put(0f);
+		vertices.put(0.6f).put(-0.4f).put(0f).put(0f).put(1f).put(0f);
+		vertices.put(0f).put(0.6f).put(0f).put(0f).put(0f).put(1f);
+		vertices.flip();
+
+		int vbo = glGenBuffers();
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW);
+
+	}
+
+	private void loadTexture() throws IOException {
+		this.tex = glGenTextures();
+		glBindTexture(GL_TEXTURE_2D, tex);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_TRUE);
+
+		IntBuffer w = BufferUtils.createIntBuffer(1);
+		IntBuffer h = BufferUtils.createIntBuffer(1);
+		IntBuffer comp = BufferUtils.createIntBuffer(1);
+
+		ByteBuffer imageBuffer = ioResourceToByteBuffer("test.png", 8 * 1024);
+		if (stbi_info_from_memory(imageBuffer, w, h, comp) == 0)
+			throw new IOException("Failed to read image information: "
+					+ stbi_failure_reason());
+		ByteBuffer image = stbi_load_from_memory(imageBuffer, w, h, comp, 0);
+		if (image == null)
+			throw new IOException("Failed to load image: "
+					+ stbi_failure_reason());
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, w.get(0), h.get(0), 0, GL_RGB,
+				GL_UNSIGNED_BYTE, image);
+		stbi_image_free(image);
 	}
 }
